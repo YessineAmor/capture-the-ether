dd3235a81cccf462a4bd6f46a6daaa6153306516
Yassine <yassine.amor@esprit.tn>
2019-09-04 20:30:07 +0100

    add crack_keccak

---
+++ guess-the-secret-number/crack_keccak.py
@@ -0,0 +1,8 @@
+from web3 import Web3, IPCProvider
+
+w3 = Web3(IPCProvider())
+print(w3.solidityKeccak(['uint8'], [50]).hex())
+
+for i in range(0,256):
+    if(w3.solidityKeccak(['uint8'], [i]).hex() == "0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365"):
+        print("hash cracked: ",i)
